73b5a9e606a1a9f27b9a0f3fb5875183ff6d4c6f
corderoangel <corderoan777@gmail.com>
2024-11-15 11:39:34 -0500

    skills seccion

--- src/Software.js
+++ src/Software.js
@@ -14,79 +14,79 @@ const Software = () => {
 				</h2>
 				<div className="skills-container_icons" align="center">
 					{/* tansformers, nvivo, spss */}
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=java" alt="Java" />
 							<p>Java</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=javascript" alt="JavaScript" />
 							<p>JavaScript</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=cpp" alt="cpp" />
 							<p>cpp</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=php" alt="php" />
 							<p>php</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=python" alt="python" />
 							<p>python</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=tensorflow" alt="tensorflow" />
 							<p>tensorflow</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=pytorch" alt="pytorch" />
 							<p>pytorch</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=nodejs" alt="nodejs" />
 							<p>nodejs</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=mongodb" alt="mongodb" />
 							<p>mongodb</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=mysql" alt="mysql" />
 							<p>mysql</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=latex" alt="latex" />
 							<p>latex</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=matlab" alt="matlab" />
 							<p>matlab</p>
 						</div>
 					</a>
-					<a href="https://skillicons.dev" onClick={handleClick}>
+					<a href="https://skillicons.dev" onClick={handleClick} className="a-icon">
 						<div className="icon-item">
 							<img src="https://skillicons.dev/icons?i=r" alt="r" />
 							<p>R</p>
